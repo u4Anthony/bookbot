72597a6401d62732e7743d631a80dace7179c916
Anthony Haddad <ahaddadkc@gmail.com>
2024-06-09 10:03:32 -0400

    get book content and book word count

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print('hello world')
+
+def main():
+    frankenstein_path = 'books/frankenstein.txt'
+    frankenstein_content = get_book_content(frankenstein_path)
+    frankenstein_word_count = get_word_count(frankenstein_content)
+    print(f'{frankenstein_word_count} words found in {frankenstein_path}')
+
+def get_word_count(contents):
+    word_count = len(contents.split())
+    return word_count
+
+def get_book_content(file_path):
+    with open(file_path) as f:
+        file_contents = f.read()
+    return file_contents
+
+if __name__ == '__main__':
+    main()
