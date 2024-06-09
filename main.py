aa52f5bf8c438244cd9c8851d36d39e6a4a555f8
Anthony Haddad <ahaddadkc@gmail.com>
2024-06-09 10:52:42 -0400

    book report method

--- main.py
+++ main.py
@@ -1,20 +1,35 @@
 
 def main():
     frankenstein_path = 'books/frankenstein.txt'
-    frankenstein_content = get_book_content(frankenstein_path)
-    frankenstein_word_count = get_word_count(frankenstein_content)
-    print(f'{frankenstein_word_count} words found in {frankenstein_path}')
-    frankenstein_char_count = get_char_count(frankenstein_content)
-    print(frankenstein_char_count)
+    book_report(frankenstein_path)
+
+def book_report(book_path):
+    book_title = book_path.split('/')[1]
+    book_content = get_book_content(book_path)
+    book_word_count = get_word_count(book_content)
+    book_char_count = get_char_count(book_content)
+    print(f'--- Begin Report of Book: {book_title}')
+    print(f'{book_word_count} words found.')
+    for char, count in book_char_count.items():
+        if char.isalpha():
+            print(f'The \'{char}\' character was found {count} times.')
+    print('--- End Book Report ---')
+
+def value_getter(item):
+    return item[1]
 
 def get_char_count(contents):
+    unsorted_char_count = {}
     char_count = {}
     for char in contents:
         lower_char = char.lower()
-        if lower_char in char_count.keys():
-            char_count[lower_char] += 1
+        if lower_char in unsorted_char_count.keys():
+            unsorted_char_count[lower_char] += 1
         else:
-            char_count[lower_char] = 1
+            unsorted_char_count[lower_char] = 1
+    sorted_char_count = sorted(unsorted_char_count.items(), key=value_getter, reverse=True)
+    for char, count in sorted_char_count:
+        char_count[char] = count
     return char_count
 
 def get_word_count(contents):
